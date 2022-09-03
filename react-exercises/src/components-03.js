64eded8b4e6952653780ad943e232077b36d7beb
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-03 14:37:03 +0200

    adding components-03

---
+++ react-exercises/src/components-03.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hello } from "./components-02";
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
