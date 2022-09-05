cfe662863449c1bbd2630b9e9fa5fa223dfdb08d
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-05 18:04:36 +0200

    add conditional-04

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component{
     render(){
         return(
             <div>
-                <Welcome  name="John" age={23} />  
+                <Welcome  name="John" age={17} />  
             </div>
         )
     }
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Age } from "./Age"
+
 export class Welcome extends React.Component{
     render(){
         return (
         <div>
             <p>Welcome, {this.props.name}</p>
-            {(this.props.age > 18 && this.props.age < 65 && this.props.name == "John")  && <Age age={this.props.age}/> }
+            <Age age={this.props.age}/>
         </div>
         )}
 }
