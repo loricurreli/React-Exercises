abd75ffae384186152b1cb76b8d855f4835c7078
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-07 19:28:10 +0200

    add events-01

---
+++ react-exercises/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import { ClickCounter } from "./ClickCounter";
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+
+root.render(<ClickCounter />);
