76609e4f927a8b64b8232325a114857feaac62ab
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-08 16:29:26 +0200

    add component-lifecycle-01

--- react-exercises/src/Counter.js
+++ react-exercises/src/Counter.js
@@ -4,17 +4,15 @@ import { CounterDisplay } from "./CounterDisplay"
 export class Counter extends React.Component{
     state = {
         count: this.props.initialCounter
-    }
-
-    constructor(props){
-        super(props)
+    }        
+    componentDidMount() {
         setInterval(() => {
             this.setState((state) => {
                 return {
                     count: this.state.count + this.props.incrementValue,
                 }})
             }, this.props.incrementDelay)
-        }
+    }
     render(){
         return <CounterDisplay count={this.state.count} />
     }
