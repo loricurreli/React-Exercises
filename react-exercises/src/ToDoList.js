7f91b6c8aeaad95bbc8967e361473ba22aecb173
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-09 20:39:11 +0200

    add lists-05

--- react-exercises/src/ToDoList.js
+++ react-exercises/src/ToDoList.js
@@ -13,6 +13,9 @@ export class ToDoList extends React.Component{
         this.setState({items: this.state.items.concat(input.value)})
         input.value=""
     }
+    clearList = () => {
+        this.setState({items: []})
+    }
     render(){
         const list = this.state.items.map((value) => <li key={value}>{value}</li>);
         return (
@@ -20,6 +23,7 @@ export class ToDoList extends React.Component{
               <ul>{list}</ul>
               <input type="text"/>
               <button onClick={this.addItem}>Add to list</button>
+              <button onClick={this.clearList}>Clear List</button>
             </div>
         );
     }
