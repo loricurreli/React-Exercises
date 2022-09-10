94c5f23f85c47de00195cb88916b85bb79130a4b
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-10 15:25:14 +0200

    add forms-01

---
+++ react-exercises/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { InteractiveWelcome } from "./InteractiveWelcome";
+
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <InteractiveWelcome/>
+            </div>
+        )
+    }
+}
---
+++ react-exercises/src/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react"
+export class Welcome extends React.Component{
+    render(){
+        return (
+        <div>
+            <p>Welcome, {this.props.name}</p>
+        </div>
+        )}
+}
