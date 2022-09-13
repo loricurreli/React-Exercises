fc81c8cf10a66f5dcc8106d67d97591a94f48f43
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-13 12:11:12 +0200

    fixinig handleClick

--- react-exercises/src/ClickCounter.js
+++ react-exercises/src/ClickCounter.js
@@ -3,10 +3,14 @@ import React, { useState } from "react";
 export function ClickCounter(){
 
     const [counter, setCounter] = useState(0);
+
+    function handleIncrement(){
+        setCounter((counter) => counter + 1)
+    }
         return(
             <div>
                 <h2>Count: {counter}</h2>
-                <button onClick={() => setCounter(counter+1)}>INCREMENTA</button>
+                <button onClick={handleIncrement}>INCREMENTA</button>
             </div>
         )
 }
