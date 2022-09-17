b7fc08ba1912be5c9eb62313526380cdb8aa4609
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-17 12:21:34 +0200

    add forms-05

--- react-exercises/src/Login.js
+++ react-exercises/src/Login.js
@@ -1,43 +1,32 @@
 import React from "react";
 
 export class Login extends React.Component{
-    constructor(props){
-        super(props)
-    }
+    
     state = {
         username: "",
         password: "",
         remember: false
     }
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.name==="checkbox" ? e.target.checked : e.target.value});
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const username = e.target.elements.username.value
+        const password = e.target.elements.password.value
+        const remember = e.target.elements.remember.checked
+        this.setState({
+            username,
+            password,
+            remember
+        })
     }
-    handleReset = () => {
-        this.setState({username: "", password: "", remember: false});
-    }
+    
     render(){
         return(
             <div>
-                <form onSubmit={this.props.onLogin}>
-                    <div>
-                    <label>
-                        Username:
-                        <input type="text" name="username" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    </div>
-                    <div>
-                    <label>
-                        Password:
-                        <input type="password" name="password" value={this.state.value} onChange={this.handleChange} />
-                    </label>                    
-                    </div>
-                    <div>
-                    <label>
-                        Remember:
-                        <input type="checkbox" name="remember" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    </div>          
-                    <button disabled={this.state.username && this.state.password ? false : true}>Login</button>  
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="username" />
+                    <input type="password" name="password" />
+                    <input type="checkbox" name="remember" />     
+                    <button>Login</button>  
                     <input type="reset" onClick={this.handleReset}/>   
                 </form>       
             </div>
