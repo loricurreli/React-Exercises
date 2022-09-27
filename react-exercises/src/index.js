d94bfa8cb5265ec4488219f0a2d2ccb6ea12a5d9
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-27 09:41:09 +0200

    add fix for react router 01

--- react-exercises/src/index.js
+++ react-exercises/src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from "react-dom"
-import { App } from "./App"
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import './index.css'
 
-
-const root = document.querySelector("#root")
-
-ReactDOM.render(<App />, root)
+const root = document.querySelector('#root');
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    root);
