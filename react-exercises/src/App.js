423f00c61223176390a1113e4d511159425551b9
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-27 12:26:02 +0200

    add component composition 02

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -2,6 +2,6 @@
 
 import { Container } from "./Container"
 export const App = () => {
-    return <Container><p>This is the children</p></Container>
+    return <Container title="Component Composition 02"><p>This is the children</p></Container>
     
 }
