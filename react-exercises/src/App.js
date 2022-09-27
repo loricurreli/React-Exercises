873b7ac0a6a11cdec34fd8adaf4d310ce8713b92
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-27 10:01:18 +0200

    add react router 04

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,13 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link} from 'react-router-dom'
 import { Welcome } from './Welcome'
 import { ClickCounter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
+import { Fragment } from 'react';
 export const App = () => {
     return (
-    <Routes>
-      <Route path="/" element={<Welcome name='Jimmy'/>} />
-      <Route path="/counter" element={<ClickCounter/>} />
-      <Route path="users/:username" element={<ShowGithubUser username='loricurreli' />} />
+        <Routes>
+        <Route path="/" element={
+        <Fragment>
+            <Welcome/>
+            <Link to="/counter">Show the counter</Link> 
+            <Link to="/users">Show the github users</Link> 
+            <Link to="/">Back to the homepage</Link>
+        </Fragment>
+    }/>
+        <Route path="/counter" element={<ClickCounter />} />
+        <Route path="/users">
+            <Route index element={<h3>Insert /username param</h3>}/>
+            <Route path=":username" element={<ShowGithubUser />} />
+        </Route>
+        
     </Routes>
     );
 }
