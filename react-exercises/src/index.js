26eb1f4f296ab9246564147bf6fe06b926c47531
Lorenzo Curreli <104564805+loricurreli@users.noreply.github.com>
2022-09-27 09:42:59 +0200

    Update index.js

--- react-exercises/src/index.js
+++ react-exercises/src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import './index.css'
 
 const root = document.querySelector('#root');
 ReactDOM.render(
