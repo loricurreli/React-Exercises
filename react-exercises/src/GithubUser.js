4b77b8ee040cdd098fb9b26724cd04480984e54e
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-28 07:00:11 +0200

    add custom-hooks-04

--- react-exercises/src/GithubUser.js
+++ react-exercises/src/GithubUser.js
@@ -2,29 +2,42 @@ import React, { useState, useEffect } from "react";
 
 export function useGitHubUser(username) {
     const [user, setUser] = useState(undefined);
+    const [error, setError] = useState(undefined);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         fetch(`https://api.github.com/users/${username}`)
-            .then(response => response.json())
-            .then(data => setUser(data))
+            .then(response => {
+                if(response.status === 200){
+                    return response.json()   
+                }
+                else setError(new Error('An error has occurred')) 
+            })
+            .then(data=> setUser(data))
+            .catch(error => setError(error))
+            .finally(() => setLoading(false))
     }, [username])
 
-    return{
-        data: user
+    return {
+        user: user,
+        error: error,
+        loading: loading
     }
 }
 
 
 export function GithubUser(props){
     
-    const {data} = useGitHubUser(props.username)
+    const {user, error, loading} = useGitHubUser(props.username)
 
     
     return (<div>
-        {data && (<div> <h1>Nome utente: {data.login}</h1> <p>Id Utente: {data.id}</p> </div>)}
+        {loading && <p>Fetching the user</p>}
+        {error && <p>User Not Found</p>}
+        {user && (<div> <h1>Nome utente: {user.login}</h1> <p>Id Utente: {user.id}</p> </div>)}
         </div>)
 }
-
 
 
 
