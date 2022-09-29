0c07af2019e5272553019f146edab88ee48a8f5b
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-09-29 07:02:03 +0200

    fix swr01

---
+++ react-exercises/src/GithubUser.js
@@ -0,0 +1,26 @@
+import useSWR from "swr"
+
+const fetchUser = (url) =>
+    fetch(url)
+        .then(response => {
+            if (response.ok) return response.json()
+            else throw new Error(('Error fetching user data.'))
+        })
+
+        
+export function useGitHubUser(username) {
+    const { data } = useSWR(`https://api.github.com/users/${username}`, fetchUser)
+
+    return {
+        data
+}}
+
+export function GithubUser({username}){
+    
+    const {data} = useGitHubUser(username)
+
+    
+    return (<div>
+        {data && (<div> <h1>Nome utente: {data.login}</h1> <p>Id Utente: {data.id}</p> </div>)}
+        </div>)
+}
